0b9d12b87e7226d06284d5fab030a0dcdac6d594
Alexander Knipfer <alexanderknipfer@gmail.com>
2022-12-26 20:18:09 -0500

    Remove ts-jest and use babel-jest and remove old types

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
   setupFiles: [require.resolve('whatwg-fetch')],
   setupFilesAfterEnv: ['<rootDir>/test/setupEnv.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
   moduleNameMapper: {
     '@/components/(.*)': '<rootDir>/components/$1',
     '@/serializers/(.*)': '<rootDir>/serializers/$1',
     '@/lib/(.*)': '<rootDir>/lib/$1',
     '@/test/(.*)': '<rootDir>/test/$1',
     '@/test-utils': '<rootDir>/utils/testUtils',
-  },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
   },
   transformIgnorePatterns: [
     '/node_modules/',
