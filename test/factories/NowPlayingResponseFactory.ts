ba8e4f74d63775f8a1f80999748337a6ce626952
Alexander Knipfer <alexanderknipfer@gmail.com>
2022-12-26 20:18:16 -0500

    Use correct faker import

--- test/factories/NowPlayingResponseFactory.ts
+++ test/factories/NowPlayingResponseFactory.ts
@@ -1,5 +1,5 @@
 import { Factory } from 'rosie';
-import faker from 'faker';
+import { faker } from '@faker-js/faker';
 import { NowPlayingResponse } from 'pages/api/now-playing';
 
 export const NowPlayingFactory = Factory.define<NowPlayingResponse>(
@@ -14,7 +14,7 @@ export const NowPlayingAttributesFactory = Factory.define<
   NowPlayingResponse['attributes']
 >('now-playing-attributes-factory')
   .attr('isPlaying', () => faker.datatype.boolean())
-  .attr('songName', () => faker.name.title())
+  .attr('songName', () => faker.random.word())
   .attr('artists', () => faker.name.firstName())
   .attr('album', () => faker.lorem.word())
   .attr('albumImage', () => faker.image.imageUrl())
