60c8d1c23cd80d113885240fcc0fed52f84ceccc
Alex <alexanderknipfer@gmail.com>
2023-01-24 10:16:05 -0500

    Change theme color meta on theme change

--- components/ThemeSwitchButton.tsx
+++ components/ThemeSwitchButton.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import MoonIcon from '../public/static/icons/moon.svg';
 import SunIcon from '../public/static/icons/sun.svg';
@@ -8,15 +8,35 @@ export default function ThemeSwitchButton() {
   const { setTheme, resolvedTheme } = useTheme();
   const [isMounted, setMounted] = useState(false);
 
-  useEffect(() => setMounted(true), []);
-
   const isDarkTheme = resolvedTheme === 'dark';
   const DarkModeSwitchIcon = isDarkTheme ? SunIcon : MoonIcon;
+
+  const setThemeContentMeta = useCallback((color: string) => {
+    document
+      .querySelector("meta[name='theme-color']")
+      .setAttribute('content', color);
+  }, []);
+
+  useEffect(() => {
+    setMounted(true);
+    // setThemeContentMeta(isDarkTheme ? '#18181b' : '#ffffff');
+  }, [setThemeContentMeta, isDarkTheme]);
+
+  // useEffect(() => {
+  //   document
+  //     .querySelector("meta[name='theme-color']")
+  //     .setAttribute('content', isDarkTheme ? '#18181b' : '#ffffff');
+  // }, []);
+
+  const handleThemeChange = () => {
+    setTheme(isDarkTheme ? 'light' : 'dark');
+    setThemeContentMeta(isDarkTheme ? '#ffffff' : '#18181b');
+  };
 
   return (
     <button
       aria-label="Toggle dark mode"
-      onClick={() => setTheme(isDarkTheme ? 'light' : 'dark')}
+      onClick={handleThemeChange}
       className="p-2 dark:text-gray-300 text-gray-700 bg-gray-200 dark:bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50"
     >
       {isMounted && <DarkModeSwitchIcon className="fill-current" />}
