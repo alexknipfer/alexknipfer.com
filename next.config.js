0f61d2e2f1282bc409ad845ad5170b41130252eb
Alex <alexanderknipfer@gmail.com>
2023-05-07 22:14:02 -0400

    Remove experimental config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@ module.exports = {
   images: {
     domains: ['i.scdn.co'],
   },
-  experimental: {
-    fontLoaders: [
-      { loader: '@next/font/google', options: { subsets: ['latin'] } },
-    ],
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
