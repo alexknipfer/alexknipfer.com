cfbf5fa204d28701ef56542507e98d2dea6c2398
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-09-13 22:41:59 -0400

    Run format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'geist-mono': 'var(--font-geist-mono)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      gridTemplateColumns: {
+        '16': 'repeat(16, 1fr)',
+        '24': 'repeat(24, 1fr)',
       },
     },
   },
